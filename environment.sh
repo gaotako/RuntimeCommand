add8ee0517ae6f33cdb11a035b45c2cd9c38ff1a
Jianfei Gao <gao462@outlook.com>
2023-09-05 15:07:40 -0700

    Refine comments.

--- environment.sh
+++ environment.sh
@@ -1,77 +1,150 @@
-#
+# Stop immediately on error.
 set -e
 
-#
+# Decide acting stages according to arguments.
+if [[ ${#} -eq 0 ]]; then
+    # By default (no arguments), perform all stages.
+    flag_install=1
+    flag_outdate=1
+else
+    # With given arguments, only execute specified actions.
+    flag_install=0
+    flag_outdate=0
+    while [[ ${#} -gt 0 ]]; do
+        #
+        case ${1} in
+        install)
+            # Act installation.
+            flag_install=1
+            ;;
+        outdate)
+            # Act outdate checking.
+            flag_outdate=1
+            ;;
+        esac
+        shift 1
+    done
+fi
+
+# Global mappings:
+# - Packages installed by this script and their versions;
+# - Packages with available latest update and their update versions;
+# - Packages ignoring available latest update and ignoring reasons.
 declare -A installed
 declare -A latests
 declare -A ignores
 
-# Using package installer for Python to maintain.
+# Interface of using package installer for Python (pip) to install.
+#
+# Args
+# ----
+# - name
+#   Python package index (PyPI) name of installing package.
+# - extra
+#   Extra dependency requirement names of installing package.
+# - version
+#   Version number of installing package.
+# - args
+#   Other arguments being directly passed to pip.
+#
+# Returns
+# -------
 install() {
-    #
+    # Local variables for argument aliases.
     local name
     local extra
     local version
 
-    #
+    # Parse arguments, and collect remaining arguments.
     name=${1}
     extra=${2}
     version=${3}
     shift 3
 
-    #
-    if [[ ${#extra} -eq 0 ]]; then
-        #
-        pip install --no-cache-dir --upgrade ${name}==${version} ${*}
-    else
-        #
-        pip install --no-cache-dir --upgrade ${name}[${extra}]==${version} ${*}
+    # Perform installation command only when installation stage is active.
+    if [[ ${flag_install} -gt 0 ]]; then
+        # Extra dependency will influence the form of pip installation command.
+        # Extra dependency only controls if some functionalities are supported, e.g., optimization, and has no influence to package version.
+        # Different extra denpendencies will still be identified as the same package version.
+        if [[ ${#extra} -eq 0 ]]; then
+            # Default installation.
+            pip install --no-cache-dir --upgrade ${name}==${version} ${*}
+        else
+            # Extra dependency installation.
+            pip install --no-cache-dir --upgrade ${name}[${extra}]==${version} ${*}
+        fi
     fi
     installed[${name}]=${version}
 }
 
+# Parse package update information of package installer for Python (pip).
 #
+# Args
+# ----
+#
+# Returns
+# -------
 outdate() {
-    #
+    # Local variables for argument aliases.
+    local lines
     local nlns
     local name
     local latest
 
-    #
+    # Get pip update information.
+    # Parse each information line.
     latests=()
+    lines="$(pip list --outdated)"
     nlns=0
     while IFS= read -r line; do
-        #
+        # Trace reading line number.
+        # First and second line are headers to be ignored.
         nlns=$((nlns + 1))
         [[ ${nlns} -gt 2 ]] || continue
 
-        #
+        # Get outdated package name and latest version.
         name=$(echo ${line} | awk "{print \$1}")
         latest=$(echo ${line} | awk "{print \$3}")
         latests[${name}]=${latest}
-    done <<<$(pip list --outdated)
+    done <<<"${lines}"
+    nlns=$((nlns - 2))
+    echo "${nlns} package(s) are outdated."
 }
 
 # Get CUDA version numbers.
+#
+# Args
+# ----
+#
+# Returns
+# -------
 getcu() {
+    # Local variables for processing CUDA version string.
+    local cu
+    local cuV
+    local msg
+
     # CUDA release number start with "V".
     cu=$(nvcc --version | grep release | awk "{ print \$NF }")
     cuV=${cu:0:1}
     if [[ ${cuV} != "V" ]]; then
-        #
+        # If the first character is not "V", directly stop this function with error raising.
         echo "error: CUDA release string is not \"V\${major}.\${minor}.\${release}\"."
         exit 1
     fi
 
     # Get major version.
+    # Pay attention that `cumajor` is global.
     cu=${cu:1}
     cumajor=${cu%%.*}
 
     # Get minor version.
+    # Pay attention that `cuminor` is global.
     cu=${cu#*.}
     cuminor=${cu%%.*}
 
     # Get release version.
+    # Pay attention that `curelease` is global.
     cu=${cu#*.}
     curelease=${cu%%.*}
 
@@ -80,54 +153,59 @@ getcu() {
         ${#cumajor} -gt 0 && ${#cuminor} -gt 0 && ${#curelease} -gt 0 &&
         ${#cu} -eq ${#curelease} ]] \
         ; then
-        #
-        msg1="Major = ${cumajor}, Minor = ${cuminor}, Release = ${curelease}"
-        echo "Detect CUDA version numbers: ${msg1}"
+        # Report major, minor and release versions.
+        msg="Major = ${cumajor}, Minor = ${cuminor}, Release = ${curelease}"
+        echo "Detect CUDA version numbers: ${msg}"
     else
-        #
+        # If the version form is not as expected, directly stop this function with error raising.
         echo "error: CUDA release string is not \"V\${major}.\${minor}.\${release}\"."
         exit 1
     fi
 }
 
-# Variables.
+# Package version related variables.
 if [[ -z $(which nvcc 2>/dev/null) ]]; then
-    #
+    # If GPU and CUDA are not active, use CPU as CUDA version for computation device.
     vercu=cpu
 else
-    #
+    # Otherwise, get CUDA version for computation device.
     getcu
     vercu=cu${cumajor}${cuminor}
 fi
 verth=2.0.0
 
-# Upgrade pip.
-pip install --no-cache-dir --upgrade pip
-pip install --no-cache-dir --upgrade setuptools
-pip install --no-cache-dir --upgrade wheel
+# Installation stage.
+if [[ ${flag_install} -gt 0 ]]; then
+    # Upgrade pip.
+    pip install --no-cache-dir --upgrade pip
+    pip install --no-cache-dir --upgrade setuptools
+    pip install --no-cache-dir --upgrade wheel
+fi
 
-#
+# Install packages regardless of stage settings.
+# Pseudo installation will be performed if installation stage is inactive.
 install black jupyter 23.7.0
 install isort "" 5.12.0
 install flake8 "" 6.1.0
-install mypy "" 1.4.1
-install pytest "" 7.4.0
+install mypy "" 1.5.1
+install pytest "" 7.4.1
 install pytest-cov "" 4.1.0
 install more-itertools "" 10.1.0
 install numpy "" 1.24.4
-install scipy "" 1.11.1
+install scipy "" 1.11.2
 install scikit-learn "" 1.3.0
 install matplotlib "" 3.7.2
-install pandas "" 2.0.3
+install pandas "" 2.1.0
 install seaborn "" 0.12.2
 install lmdb "" 1.4.1
-install ray "" 2.6.2
+install ray "" 2.6.3
 install numba "" 0.57.1
 install textdistance extras 4.5.0
-install datasketch "" 1.5.9
+install datasketch "" 1.6.0
 install tabulate "" 0.9.0
 
-#
+# Install deep learning packages.
+# Pseudo installation will be performed if installation stage is inactive.
 install torch "" ${verth} --extra-index-url https://download.pytorch.org/whl/${vercu}
 install pyg_lib "" 0.2.0 -f https://data.pyg.org/whl/torch-${verth}+${vercu}.html
 install torch-scatter "" 2.1.1 -f https://data.pyg.org/whl/torch-${verth}+${vercu}.html
@@ -136,30 +214,35 @@ install torch-cluster "" 1.6.1 -f https://data.pyg.org/whl/torch-${verth}+${verc
 install torch-spline-conv "" 1.2.2 -f https://data.pyg.org/whl/torch-${verth}+${vercu}.html
 install torch-geometric "" 2.3.0 -f https://data.pyg.org/whl/torch-${verth}+${vercu}.html
 
-#
-ignores["numpy"]=true
-ignores["torch"]=true
-ignores["torch-geometric"]=true
+# Register update ignoring packages and reasons.
+ignores["numpy"]="For Numba support."
+ignores["torch"]="Use stable version."
+ignores["torch-geometric"]="Use stable version."
 
-#
-outdate
-for package in ${!installed[@]}; do
-    #
-    if [[ -n ${latests[${package}]} ]]; then
-        #
-        if [[ -z ${ignores[${package}]} ]]; then
-            # If outdated package is not ignored, explicitly mark it is outdated.
-            msg1="\x1b[1;93m${package}\x1b[0m"
-            msg2="\x1b[2;94m${installed[${package}]}\x1b[0m"
-            msg3="${msg1} (${msg2}) is \x1b[4;93moutdated\x1b[0m"
-            msg4="latest version is \x1b[94m${latests[${package}]}\x1b[0m"
-        else
-            # Otherwise, report that it is forced to current version.
-            msg1="\x1b[92m${package}\x1b[0m"
-            msg2="\x1b[94m${installed[${package}]}\x1b[0m"
-            msg3="${msg1} (${msg2}) is \x1b[92mforced\x1b[0m"
-            msg4="latest version is \x1b[2;94m${latests[${package}]}\x1b[0m"
+# Outdate checking stage.
+if [[ ${flag_outdate} -gt 0 ]]; then
+    # Collect all outdated packages.
+    outdate
+
+    # Traverse every packages installed by this script.
+    for package in ${!installed[@]}; do
+        # Capture outdated ones among those packages.
+        if [[ -n ${latests[${package}]} ]]; then
+            # Outdate report varies according to ignoring settings.
+            if [[ -z ${ignores[${package}]} ]]; then
+                # If outdated package is not ignored, explicitly mark it as outdated.
+                msg1="\x1b[1;93m${package}\x1b[0m"
+                msg2="\x1b[2;94m${installed[${package}]}\x1b[0m"
+                msg3="${msg1} (${msg2}) is \x1b[4;93moutdated\x1b[0m"
+                msg4="latest version is \x1b[94m${latests[${package}]}\x1b[0m"
+            else
+                # Otherwise, report that it is forced to current version, and report reason.
+                msg1="\x1b[92m${package}\x1b[0m"
+                msg2="\x1b[94m${installed[${package}]}\x1b[0m"
+                msg3="${msg1} (${msg2}) is \x1b[92mforced\x1b[0m"
+                msg4="latest version is \x1b[2;94m${latests[${package}]}\x1b[0m"
+            fi
+            echo -e "${msg3} (${msg4})."
         fi
-        echo -e "${msg3} (${msg4})."
-    fi
-done
+    done
+fi
