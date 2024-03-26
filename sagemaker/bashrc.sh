46e7cea8c2522f821bb894ee02416f0a100cabb7
Jianfei Gao <gajianfe@amazon.com>
2024-03-26 22:29:05 +0000

    hotfix.

--- sagemaker/bashrc.sh
+++ sagemaker/bashrc.sh
@@ -7,15 +7,15 @@ export VIRENV="${HOME}/SageMaker/${PROJECT}/conda/${STAGE}"
 # Raise error if runtime information is not fully provided.
 if [[ -z ${HOME} ]]; then
     # Focusing project name for SageMaker running should be defined.
-    echo -e "\033[96mwarning\033[0m: Home directory has not been claimed for bashrc."
+    echo -e "\033[95mwarning\033[0m: Home directory has not been claimed for bashrc."
 fi
 if [[ -z ${PROJECT} ]]; then
     # Focusing project name for SageMaker running should be defined.
-    echo -e "\033[96mwarning\033[0m: SageMaker focusing project name in not defined in bashrc."
+    echo -e "\033[95mwarning\033[0m: SageMaker focusing project name in not defined in bashrc."
 fi
 if [[ -z ${STAGE} ]]; then
     # Focusing project working stage should be defined.
-    echo -e "\033[96mwarning\033[0m: SageMaker focusing project working stage in not defined in bashrc."
+    echo -e "\033[95mwarning\033[0m: SageMaker focusing project working stage in not defined in bashrc."
 fi
 
 # Customized runtime commands.
